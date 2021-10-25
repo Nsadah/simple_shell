1016e86ae0b5d8a294e87db3e33fcc0de7a68bab
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-10-26 01:12:39 +0300

    Create main.h

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*Description: This is the header file for the project work
+*/
+
+int _isupper(int c);
+
+#endif /* MAIN_H */
