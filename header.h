de726082d0efb96b66a39d215ecf80d707a81a91
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-10-28 00:18:55 +0300

    this is the header file that shall be used

---
+++ header.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_H
+#define HEADER_H
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*Description: This is the header file for the project work
+*/
+
+int _isupper(int c);
+
+
+#endif /* HEADER_H */
