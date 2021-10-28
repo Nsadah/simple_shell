73703aeb0c0a7d1a6e48bf302bd43a21eeaae4d2
henschel05 <henschelzulu05@gmail.com>
2021-10-28 14:47:57 -0700

    header file

---
+++ header.h
@@ -0,0 +1,12 @@
+#ifndef HEADER
+#define HEADER
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int _isupper(int c);
+int main(int ac, char **av, char **envp)
+
+#endif
