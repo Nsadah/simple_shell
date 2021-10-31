08b0d1f69f54c905965d5c02bdf0e56509ffe264
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-11-01 01:11:45 +0300

    Update betty.c

--- betty.c
+++ betty.c
@@ -8,3 +8,13 @@ int _isupper(int c)
 {
 return (c >= 'A' && c <= 'Z');
 }
+int main(void)
+{
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
+}
