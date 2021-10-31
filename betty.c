d86172b68223bdb9bba3f371275b40ff26ddf87d
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-11-01 01:13:30 +0300

    Update betty.c

--- betty.c
+++ betty.c
@@ -10,11 +10,10 @@ return (c >= 'A' && c <= 'Z');
 }
 int main(void)
 {
-    char c;
-
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+char c;
+c = 'A';
+printf("%c: %d\n", c, _isupper(c));
+c = 'a';
+printf("%c: %d\n", c, _isupper(c));
+return (0);
 }
