1e501bd8467d6a8bf0829e6cb9f769c8ff2dcede
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-10-31 22:50:11 +0300

    Update sample_shell.c

--- sample_shell.c
+++ sample_shell.c
@@ -250,18 +250,18 @@ int main()
     init_shell();
   
     while (1) {
-        // print shell line
+        /* print shell line */
         printDir();
-        // take input
+        /* take input */
         if (takeInput(inputString))
             continue;
-        // process
+        /* process */
         execFlag = processString(inputString,
         parsedArgs, parsedArgsPiped);
-        // execflag returns zero if there is no command
-        // or it is a builtin command,
-        // 1 if it is a simple command
-        // 2 if it is including a pipe.
+        /* execflag returns zero if there is no command */
+        /* or it is a builtin command, */
+        /* 1 if it is a simple command */
+        /* 2 if it is including a pipe. */
   
         // execute
         if (execFlag == 1)
